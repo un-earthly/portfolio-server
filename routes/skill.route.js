6768c0c14df0f76f0033946237958129954a879e
MD. Alamin <vijayalamin@gmail.com>
2023-07-21 15:28:12 +0600

    wrong push solved

--- routes/skill.route.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const skillController = require("../controllers/skill.controller")
-
-router.get('/list', skillController.getSkillList);
-router.post('/insert', skillController.insertSkill);
-router.patch('/update/:id', skillController.updateSkill);
-router.delete('/delete/:id', skillController.deleteSkill);
-module.exports = router
